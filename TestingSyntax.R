3bf627d39d2ad57c5dd0dba057b453bbeeb3ff81
Murilo Moraes <murilo.alves8@gmail.com>
2020-08-15 02:39:50 -0300

    object class study

--- TestingSyntax.R
+++ TestingSyntax.R
@@ -1,5 +1,27 @@
 x <- 58
 x
 
-msg <- "Coursera" ##just a string test
+msg <- "Coursera" #just a string test
 msg
+
+y <- 4:16
+y[12]
+
+z <- Inf
+
+x/z
+
+attributes(x)
+
+fcn <- c(0.4,0.7) #function c numeric
+fcn
+
+fcc <- c(1+4i,2+6i) #function c complex
+
+v <- vector("complex", length = 6)
+v
+
+class(v)
+as.character(v)
+as.integer(v)
+as.logical(v)
