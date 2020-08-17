424699bc754f1a99dc8ad92c1f3af138aeb4c360
Murilo Moraes <murilo.alves8@gmail.com>
2020-08-17 08:08:47 -0300

    dput and dump testing

--- Swirltest.R
+++ Swirltest.R
@@ -3,3 +3,22 @@ install_from_swirl("R Programming")
 swirl()
 
 x <- 7
+v <- vector("complex", length = 6)
+v
+
+
+y <- data.frame(a=1, b="a")
+y
+dput(y)
+
+dput(y, file="y.R")
+new.y <- dget("y.R")
+new.y
+
+dump(c("x","v","y"), file = "data.R")
+rm(x,v,y)
+
+source("data.R")
+x
+v
+y
